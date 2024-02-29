466a188aec032044f19c8936ce9b76b4a21c6f5c
Fatikah002 <sfatikah2005@gmail.com>
2024-02-29 09:24:24 +0700

    P2_percobaan2_Pertanyaan4_1C_12

--- P2/penjualanBuku/BukuMain12.java
+++ P2/penjualanBuku/BukuMain12.java
@@ -8,11 +8,10 @@ public class BukuMain12 {
         bk1.halaman = 198;
         bk1.stok = 13;
         bk1.harga = 71000;
-
-        bk1.tampilInformasi();
+        
         bk1.terjual(13);
-        bk1.gantiHarga(60000);
         bk1.tampilInformasi();
+        bk1.gantiHarga(60000);
      
     }
 }
