f2ea80e004442107c12ea78dfdd7b449df6b067c
Fatikah002 <sfatikah2005@gmail.com>
2024-02-29 10:55:57 +0700

    P2_percobaan3_1C_12

--- P2/penjualanBuku/BukuMain12.java
+++ P2/penjualanBuku/BukuMain12.java
@@ -3,29 +3,25 @@ package P2.penjualanBuku;
 public class BukuMain12 {
     public static void main(String[] args) {
       
-        Buku12 bk1 = new Buku12();
+        Buku12 bk1 = new Buku12("Today Ends Tomorrow Comes", "Denada Pratiwi", 198, 13, 71000);
         bk1.judul = "How to Train Your Dragon";
         bk1.pengarang = "Cressida Cowell";
         bk1.halaman = 432;
         bk1.stok = 13;
         bk1.harga = 275000;
 
-    
         bk1.tampilInformasi();
         bk1.terjual(5);
         bk1.gantiHarga(60000);
         bk1.tampilInformasi();
         
-
-       
-        Buku12 bk2= new Buku12();
+        Buku12 bk2 = new Buku12("How to Train Your Dragon", "Cressida Cowell", 432, 0, 275000);
         bk2.judul = "How to Train Your Dragon";
         bk2.pengarang = "Cressida Cowell";
         bk2.halaman = 432;
         bk2.stok = 0;
         bk2.harga = 275000;
 
-        
         bk2.terjual(4);
         bk2.tampilInformasi();
     }
