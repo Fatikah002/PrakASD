ba9744571b3dc8448cd456c393373899d8057ba7
Fatikah002 <sfatikah2005@gmail.com>
2024-02-29 09:35:37 +0700

    P2_percobaan2_Pertanyaan5_1C_12

--- P2/penjualanBuku/BukuMain12.java
+++ P2/penjualanBuku/BukuMain12.java
@@ -9,8 +9,20 @@ public class BukuMain12 {
         bk1.stok = 13;
         bk1.harga = 71000;
         
-        bk1.terjual(14);
+        bk1.tampilInformasi();
+        bk1.terjual(8);
         bk1.gantiHarga(60000);
         bk1.tampilInformasi();
+
+        Buku12 bk2 = new Buku12();
+        bk2.judul = "How to Train Your Dragon";
+        bk2.pengarang = "Cressida Cowell";
+        bk2.halaman = 432;
+        bk2.stok = 0;
+        bk2.harga = 275000;
+
+        bk2.terjual(0);
+        bk2.tampilInformasi();
+        bk2.gantiHarga(60000);
     }
 }
