689f6aa3cde405cf194b12564e3b414e3099e505
Fatikah002 <sfatikah2005@gmail.com>
2024-03-07 10:11:50 +0700

    P3_percobaan3_Pertanyaan4_1C_12

--- P3/ArrayBalok/Segitiga12.java
+++ P3/ArrayBalok/Segitiga12.java
@@ -16,6 +16,15 @@ public class Segitiga12 {
         double sisiMiring = Math.sqrt(alas * alas + tinggi * tinggi);
         return alas + tinggi + sisiMiring;
     }
+    public static void main(String[] args) {
+        Segitiga12[] sgArray12 =  new Segitiga12[4];
+        sgArray12[0] = new Segitiga12(10, 4);
+        sgArray12[1] = new Segitiga12(20, 10);
+        sgArray12[2] = new Segitiga12(15, 6);
+        sgArray12[3] = new Segitiga12(25, 10);
 
-   
+        for (int i = 0; i < 4; i++) {
+            System.out.println("sgArray ke-" + i + "    alas: " + sgArray12[i].alas + ", tinggi: " + sgArray12[i].tinggi);
+    }
+  }
 }
