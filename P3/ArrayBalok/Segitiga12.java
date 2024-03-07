bedc9feaebb4c188226e594b12a5155b2e85a867
Fatikah002 <sfatikah2005@gmail.com>
2024-03-07 10:08:49 +0700

    P3_percobaan3_Pertanyaan3_1C_12

--- P3/ArrayBalok/Segitiga12.java
+++ P3/ArrayBalok/Segitiga12.java
@@ -8,4 +8,14 @@ public class Segitiga12 {
         alas = a;
         tinggi = t;
     }
+
+    public double hitungLuas(){
+        return 0.5 * alas * tinggi;
+    }
+    public double hitungKeliling(){
+        double sisiMiring = Math.sqrt(alas * alas + tinggi * tinggi);
+        return alas + tinggi + sisiMiring;
+    }
+
+   
 }
