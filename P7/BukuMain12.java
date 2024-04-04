ed7c38ed1c7dd926a1ba96e26877e98b27a4ada2
Fatikah002 <sfatikah2005@gmail.com>
2024-04-04 10:15:36 +0700

    P7_Percobaan2_Pertanyaan3_1C_12

--- P7/BukuMain12.java
+++ P7/BukuMain12.java
@@ -10,10 +10,10 @@ public class BukuMain12 {
 
         System.out.println("______________________________________________");
         System.out.println("Masukkan data Buku secara Urut dari kodeBuku Terkecil : ");
-        Buku12 m = new Buku12(111,"Algoritma" , 2019, "Wahyuni", 5);
+        Buku12 m = new Buku12(125,"Algoritma" , 2019, "Wahyuni", 5);
         Buku12 m1 = new Buku12(123, "Big Data", 2020, "Susilo", 3);
-        Buku12 m2 = new Buku12(125, "Desain UI", 2021, "Supriadi", 3);
-        Buku12 m3 = new Buku12(126, "Web Programming", 2022, "Pustaka Adi", 2);
+        Buku12 m2 = new Buku12(126, "Desain UI", 2021, "Supriadi", 3);
+        Buku12 m3 = new Buku12(111, "Web Programming", 2022, "Pustaka Adi", 2);
         Buku12 m4 = new Buku12(127, "Etika Mahasiswa", 2023, "Darmawan Adi", 2);
 
         data.tambah(m);
