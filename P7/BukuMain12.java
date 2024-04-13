202cd47cf332ede98e3d9f228ffca041c00869f6
Fatikah002 <sfatikah2005@gmail.com>
2024-04-13 10:47:42 +0700

    P7_LatihanPraktikum2a_1C_12

--- P7/BukuMain12.java
+++ P7/BukuMain12.java
@@ -10,17 +10,18 @@ public class BukuMain12 {
 
         System.out.println("______________________________________________");
         System.out.println("Masukkan data Buku secara Urut dari kodeBuku Terkecil : ");
-        Buku12 m = new Buku12("127","Algoritma" , 2019, "Wahyuni", 5);
-        Buku12 m1 = new Buku12("126", "Big Data", 2020, "Susilo", 3);
+        Buku12 m = new Buku12("111","Algoritma" , 2019, "Wahyuni", 5);
+        Buku12 m1 = new Buku12("123", "Big Data", 2020, "Susilo", 3);
         Buku12 m2 = new Buku12("125", "Desain UI", 2021, "Supriadi", 3);
-        Buku12 m3 = new Buku12("123", "Web Programming", 2022, "Pustaka Adi", 2);
-        Buku12 m4 = new Buku12("111", "Etika Mahasiswa", 2023, "Darmawan Adi", 2);
-
+        Buku12 m3 = new Buku12("126", "Web Programming", 2022, "Pustaka Adi", 2);
+        Buku12 m4 = new Buku12("127", "Etika Mahasiswa", 2023, "Darmawan Adi", 2);
+        
         data.tambah(m);
         data.tambah(m1);
         data.tambah(m2);
         data.tambah(m3);
         data.tambah(m4);
+        data.bubbleSortJudul();
 
         System.out.println("______________________________________________");
         System.out.println("Data keseluruhan Buku : ");
@@ -29,23 +30,40 @@ public class BukuMain12 {
         System.out.println("______________________________________________");
         System.out.println("______________________________________________");
         System.out.println("Pencarian Data: ");
-        System.out.println("Masukkan Kode Buku yang dicari: ");
-        System.out.print("Kode Buku : ");
-        String cari = s.nextLine();
-        System.out.println("menggunakan sequential Search");
+        // System.out.println("Masukkan Kode Buku yang dicari: ");
+        // System.out.print("Kode Buku : ");
+        // String cari = s.nextLine();
+        // System.out.println("menggunakan sequential Search");
         // Buku12 bukuDitemukan = data.findBuku(cari);
-        int posisi = data.findSeqSearch(cari);
-        data.tampilPosisi(cari, posisi);
-        data.tampilData(cari, posisi);
+        // int posisi = data.findSeqSearch(cari);
+        // data.tampilPosisi(cari, posisi);
+        // data.tampilData(cari, posisi);
         
         // if (bukuDitemukan != null) {
         //     bukuDitemukan.tampilDataBuku();
         // } 
 
-        System. out. println("=============================");
-        System.out.println("menggunakan binary Search");
-        posisi = data.findBinarySearch(cari, 0, jumBuku - 1);
+        // System. out. println("=============================");
+        // System.out.println("menggunakan binary Search");
+        // posisi = data.findBinarySearch(cari, 0, jumBuku - 1);
+        // data.tampilPosisi(cari, posisi);
+        // data.tampilData(cari, posisi);
+
+        System.out.println("Masukkan Judul Buku yang dicari: ");
+        System.out.print("Judul Buku : ");
+        String cari = s.nextLine();
+
+        System.out.println("menggunakan sequential Search");
+        int posisi = data.findSeqSearchJudul(cari);
         data.tampilPosisi(cari, posisi);
         data.tampilData(cari, posisi);
+
+        System.out.println("=============================");
+        System.out.println("menggunakan binary Search");
+        posisi = data.findBinarySearchJudul(cari, 0, jumBuku - 1);
+        data.tampilPosisi(cari, posisi);
+        data.tampilData(cari, posisi);
+
+
     }
 }
--- P7/PencarianBuku12.java
+++ P7/PencarianBuku12.java
@@ -19,17 +19,6 @@ public class PencarianBuku12 {
         }
     }
 
-    // public int findSeqSearch(int cari){
-    //     int posisi = -1;
-    //     for (int j = 0; j < listBk.length; j++) {
-    //         if (listBk[j].kodeBuku == cari) {
-    //             posisi  = j ;
-    //             break;
-    //         }
-    //     }
-    // return posisi;
-    // }
-
     public int findSeqSearch(String cari) {
         int posisi = -1;
         for (int j = 0; j < listBk.length; j++) {
@@ -52,24 +41,15 @@ public class PencarianBuku12 {
 
     public void tampilData(String x, int pos){
         if (pos != -1) {
-            System.out.println("Kode Buku   \t : " +x );
-            System.out.println("Judul       \t : "+listBk[pos]. judulBuku);
-            System.out.println("Tahun Terbit\t : "+listBk[pos]. tahunTerbit);
-            System.out.println("Pengarang   \t : "+listBk[pos]. pengarang);
-            System.out.println("Stock       \t : "+listBk[pos].stock);
+            System.out.println("Kode Buku   \t : " + listBk[pos].kodeBuku);
+            System.out.println("Judul       \t : " + x);
+            System.out.println("Tahun Terbit\t : " + listBk[pos].tahunTerbit);
+            System.out.println("Pengarang   \t : " + listBk[pos].pengarang);
+            System.out.println("Stock       \t : " + listBk[pos].stock);
         } else {
             System.out.println("data " + x + " tidak ditemukan");
         }
     }
-
-    // public Buku12 findBuku(int cari) {
-    //     for (Buku12 buku : listBk) {
-    //         if (buku != null && buku.kodeBuku == cari) {
-    //             return buku;
-    //         } 
-    //     }
-    //      return null;
-    // }
 
     public Buku12 findBuku(String cari) {
         for (Buku12 buku : listBk) {
@@ -79,21 +59,6 @@ public class PencarianBuku12 {
         }
         return null;
     }
-
-//     public int FindBinarySearch(int cari, int left, int right) {
-//         int mid;
-//         if (right >= left) {
-//         mid = (left + right) / 2;
-//         if (cari == listBk[mid]. kodeBuku) {
-//             return (mid);
-//         } else if (listBk[mid]. kodeBuku < cari) {
-//             return FindBinarySearch(cari, left, mid - 1);
-//         } else {
-//             return FindBinarySearch(cari, mid + 1, right);
-//         }
-//     }
-//         return -1;
-//     }
 
     public int findBinarySearch(String cari, int left, int right) {
         int mid;
@@ -110,5 +75,44 @@ public class PencarianBuku12 {
             }
         }
         return -1;
+    }
+    
+    public int findSeqSearchJudul(String cari) {
+        int posisi = -1;
+        for (int j = 0; j < listBk.length; j++) {
+            if (listBk[j].judulBuku.equalsIgnoreCase(cari)) {
+                posisi = j;
+            }
+        }
+        return posisi;
+    }
+
+    public int findBinarySearchJudul(String cari, int left, int right) {
+        int mid;
+        if (right >= left) {
+            mid = (left + right) / 2;
+            int compare = listBk[mid].judulBuku.compareToIgnoreCase(cari);
+
+            if (compare == 0) {
+                return mid;
+            } else if (compare < 0) {
+                return findBinarySearchJudul(cari, mid + 1, right);
+            } else {
+                return findBinarySearchJudul(cari, left, mid - 1);
+            }
+        }
+        return -1;
+    }
+
+    public void bubbleSortJudul() {
+        for (int i = 0; i < listBk.length - 1; i++) {
+            for (int j = 0; j < listBk.length - i - 1; j++) {
+                if (listBk[j].judulBuku.compareToIgnoreCase(listBk[j + 1].judulBuku) > 0) {
+                    Buku12 temp = listBk[j];
+                    listBk[j] = listBk[j + 1];
+                    listBk[j + 1] = temp;
+                }
+            }
+        }
     }
 }
