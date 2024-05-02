d983a98899107078b0d9a101c203ad24c2eba29a
Fatikah002 <sfatikah2005@gmail.com>
2024-05-02 11:34:04 +0700

    P10_Percobaan2_1C_12_Revisi

--- P10/Praktikum2/QueueMain12.java
+++ P10/Praktikum2/QueueMain12.java
@@ -58,8 +58,9 @@ public class QueueMain12 {
                     antri.print();
                     break;
 
+            
             }
 
-        } while (pilih == 1 || pilih == 2 || pilih == 3 || pilih == 4 || pilih == 5);
+        } while (pilih == 1 || pilih == 2 || pilih == 3 || pilih == 4 );
     }
 }
