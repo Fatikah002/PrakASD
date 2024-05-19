406b547b7a9077aeed6defc4210ab6dbb1f17aec
Fatikah002 <sfatikah2005@gmail.com>
2024-05-19 14:53:42 +0700

    P12_Percobaan1_1C_12

---
+++ P12/DoubleLinkedLists/DoubleLinkedListMain12.java
@@ -0,0 +1,26 @@
+package P12.DoubleLinkedLists;
+
+public class DoubleLinkedListMain12 {
+    public static void main(String[] args) throws Exception {
+        
+        DoubleLinkedLists12 dll = new DoubleLinkedLists12();
+
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("====================================");
+        dll.addFirst(3);
+        dll.addLast(4);
+        dll.addFirst(7);
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("====================================");
+        dll.add(40, 1);
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("====================================");
+        dll.clear();
+        dll.print();
+        System.out.println("Size : " + dll.size());
+
+    }
+}
---
+++ P12/DoubleLinkedLists/Node12.java
@@ -0,0 +1,12 @@
+package P12.DoubleLinkedLists;
+
+public class Node12 {
+    int data;
+    Node12 prev, next;
+
+    Node12(Node12 prev, int data, Node12 next){
+        this.prev = prev;
+        this.data = data;
+        this.next = next;
+    }
+}
